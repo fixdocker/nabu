1aeec8f0ed462fa7225ac188a37f3ae4362fed2a
Murray-Luke Peard <murray-luke.peard@sydney.edu.au>
2019-05-24 16:44:30 +1000

    Updating deployment settings for staging. Use Git over SSH.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-server '115.146.80.168', :app, :web, :db, :primary => true
-set :user, 'ubuntu'
+server 'mpeada00037.srv.sydney.edu.au', :app, :web, :db, :primary => true
+set :user, 'deploy'
 
 set :unicorn_env, 'staging'
 
-set :branch, 'develop'
+set :branch, 'staging_setup'
